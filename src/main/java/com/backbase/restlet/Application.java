bacb74e52146f0f18c0c6ed82d75ed0c633e5337
Zoltan Altfatter <altfatterz@gmail.com>
2013-10-23 11:17:22 +0200

    fix typo

--- src/main/java/com/backbase/restlet/Application.java
+++ src/main/java/com/backbase/restlet/Application.java
@@ -17,7 +17,7 @@ public class Application extends org.restlet.Application {
 
         // the only way to disable the following INFO line:
         // 10:33:24.669 [Restlet-79123022] INFO  LogService - 2013-10-23	10:33:24	0:0:0:0:0:0:0:1
-        // if you know a better way, please for this example and send a pull request
+        // if you know a better way, please fork this example and send a pull request
         comp.getLogService().setEnabled(false);
 
         comp.getServers().add(Protocol.HTTP, 3000);
